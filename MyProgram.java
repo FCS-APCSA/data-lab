50ac11beee8c31c49389df5639ed1e84201f7a70
ofeng98 <82614812+ofeng98@users.noreply.github.com>
2021-05-13 23:47:50 -0700

    Update MyProgram.java

--- MyProgram.java
+++ MyProgram.java
@@ -13,8 +13,8 @@ public class MyProgram
         //if the user types all, the program compares all WorldBank objects in the arraylist in Health.java and finds the WorldBank object
         //with the highest medical expenditures in terms of % GDP and per capita.
         if(country.equals("all")){
-            worldbank.highestPercentGDP();
-            worldbank.highestPerCapita();
+            worldbank.lowestPercentGDP();
+            worldbank.lowestPerCapita();
         }else{
         //otherwise, the program runs these methods to see if there is a WorldBank object with the specified name and returns its value.
             worldbank.findPercentGDP(country);
